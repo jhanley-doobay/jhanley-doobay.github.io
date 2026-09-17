a3d93983798090cd56a6c103356374a926dab90d
jhanley-doobay <jhanley@doobay.org>
2026-09-17 16:29:27 -0400

    Looks great. besides missing styling

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,16 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("posts", (collection) => {
-    return collection.getFilteredByGlob("posts/**.*");
+    return collection.getFilteredByGlob("posts/**/*.njk")
+      .filter((item) => !item.data.draft && !item.fileSlug.startsWith("_"))
+      .sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addNunjucksFilter("dateFormat", (date, format) => {
+    const months = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, "0");
+    return `${months[d.getMonth()]} ${day}, ${d.getFullYear()}`;
   });
 
   eleventyConfig.addPassthroughCopy({ "images": "images" });
